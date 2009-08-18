78becac94e0397d31e2a08ef41468cba6967d1e3
Franck PATTE <fpatte@franck-pattes-macbook.local>
2009-08-18 19:47:17 +0200

    testing rpxnow

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect '/access_restricted' , :controller => 'users', :action => 'access_restricted'
   map.connect '/login' , :controller => 'users', :action => 'login'
+  map.connect '/logout' , :controller => 'users', :action => 'logout'
+
   map.connect "/rpx_token_sessions_url", :controller => 'users', :action => 'rpx_token_sessions_url'
 
   # The priority is based upon order of creation: first created -> highest priority.
