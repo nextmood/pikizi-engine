1b1ba38e4dfabf01de1b6cb152a1ffcea78b0801
Franck PATTE <fpatte@franck-pattes-macbook.local>
2009-11-18 22:58:34 +0100

    merging layout

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,7 +3,7 @@ class LandingController < ApplicationController
   skip_before_filter :check_user_authorization, :only => ['index']
 
   def index
-    render :layout => 'landing' 
+    render :layout => false
   end
 
 end
