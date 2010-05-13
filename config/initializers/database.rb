09400124fa3612069a000dc5b50364491ed75c57
Franck <franck@nextmood.com>
2010-05-13 08:42:14 +0200

    bug

--- config/initializers/database.rb
+++ config/initializers/database.rb
@@ -2,8 +2,8 @@ MongoMapper.database = "pikizi_mongodb_#{Rails.env}"
 
 #MongoMapper.database = "pikizi_mongodb_development"
 
-#if defined?(PhusionPassenger)
-#   PhusionPassenger.on_event(:starting_worker_process) do |forked|
-#     MongoMapper.connection.connect_to_master if forked
-#   end
-#end
+if defined?(PhusionPassenger)
+   PhusionPassenger.on_event(:starting_worker_process) do |forked|
+     MongoMapper.connection.connect_to_master if forked
+   end
+end
