5621fd01d53924888cc71ce7bad9b8ad893af1b9
Franck PATTE <fpatte@Franck-PATTEs-iMac-2.local>
2010-05-13 15:12:47 +0200

    bug

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -349,10 +349,17 @@ class Knowledge < Root
     raise "dimension_idurl=#{dimension_idurl}" unless dimension
     all_products = products.select { |p| dimension.confidence(p) > ranking_threshold }
 
-    all_products.sort! {|p1, p2| p2.get_value(dimension.idurl) <=> p1.get_value(dimension.idurl) }
+    all_products.sort! do |p1, p2|
+      p1_rating = p1.get_value(dimension.idurl)
+      p1_rating = 0.0 if p1_rating.nan?
+      p2_rating = p2.get_value(dimension.idurl)
+      p2_rating = 0.0 if p2_rating.nan?
+      p2_rating <=> p1_rating
+    end
+
     ProductsByShortcut.shortcuts.inject([]) do |l, (shortcut, shortcut_label)|
       pf = ProductsByShortcut.new(:shortcut_selector => shortcut)
-      sub_products = pf.generate_matching_products(all_products)
+      sub_products = pf.compute_matching_product(all_products)
       l <<  [shortcut, shortcut_label, sub_products]
       l
     end
